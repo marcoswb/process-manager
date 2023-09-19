cb4b7d673d6984b3f5657ee1a15f1dc76fac1b83
Marcos Warmling Berti <marcos_wb@outlook.com>
2023-09-19 20:09:22 -0300

    show informations user

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from models.Process import Process
 
-all_processes = ['1311']
+all_processes = ['1084']
 
 for pid in all_processes:
     proc = Process(pid)
-    print(proc.pid)
-    print(proc.user)
-    print(proc.user_id)
-    print(proc.cpu_usage)
-    print(proc.memory_usage)
-    print(proc.disk_read)
-    print(proc.disk_write)
-    print(proc.priority)
+    print(f'PID: {proc.pid}')
+    print(f'USER: {proc.user}')
+    print(f'USER ID: {proc.user_id}')
+    print(f'CPU USE: {proc.cpu_usage}%')
+    print(f'MEMORY USE: {proc.memory_usage} KB')
+    print(f'READ DISK: {proc.disk_read}%')
+    print(f'WRITE DISK: {proc.disk_write}%')
+    print(f'PRIORITY: {proc.priority}')
