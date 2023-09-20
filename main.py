0b2f629e4ab77541928ecce517ceda242606b8b8
Marcos Warmling Berti <marcos_wb@outlook.com>
2023-09-19 21:03:50 -0300

    list all process

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
-from os import system
+from os import system, listdir
+from os.path import isdir
 from models.Process import Process
+from utils.functions import *
 
 all_processes = ['1983']
 
 system('clear')
-for pid in all_processes:
-    proc = Process(pid)
-    print(f'NAME: {proc.name}')
-    print(f'PID: {proc.pid}')
-    print(f'NAME: {proc.status}')
-    print(f'USER: {proc.user}')
-    print(f'USER ID: {proc.user_id}')
-    print(f'CPU USE: {proc.cpu_usage}%')
-    print(f'MEMORY USE: {proc.memory_usage} KB')
-    print(f'READ DISK: {proc.disk_read} KB')
-    print(f'WRITE DISK: {proc.disk_write} KB')
-    print(f'PRIORITY: {proc.priority}')
+print('NAME   |', end='')
+print('PID   |', end='')
+print('STATUS   |', end='')
+print('USER   |', end='')
+print('USER ID   |', end='')
+print('CPU USE   |', end='')
+print('MEMORY USE   |', end='')
+print('READ DISK   |', end='')
+print('WRITE DISK   |', end='')
+print('PRIORITY   |', end='')
+print('')
+for directory in listdir('/proc'):
+    if not isdir(f'/proc/{directory}') or not format_int(directory):
+        continue
+
+    proc = Process(directory)
+    print(f'{proc.name}   |', end='')
+    print(f'{proc.pid}   |', end='')
+    print(f'{proc.status}   |', end='')
+    print(f'{proc.user}   |', end='')
+    print(f'{proc.user_id}   |', end='')
+    print(f'{proc.cpu_usage}%   |', end='')
+    print(f'{proc.memory_usage} KB   |', end='')
+    print(f'{proc.disk_read} KB   |', end='')
+    print(f'{proc.disk_write} KB   |', end='')
+    print(f'{proc.priority}   |', end='')
+    print('')
