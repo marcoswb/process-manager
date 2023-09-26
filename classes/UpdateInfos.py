33259c7abc2a544f1b6aaa54b09b8c46fbff77f0
Marcos Warmling Berti <marcos_wb@outlook.com>
2023-09-26 20:58:48 -0300

    refactor update infos memory and status process

--- classes/UpdateInfos.py
+++ classes/UpdateInfos.py
@@ -1,19 +1,46 @@
 from PySide6.QtCore import QThread, Signal
 from time import sleep
+from os import listdir
+from os.path import isdir
 
+from models.Process import Process
 from utils.functions import *
 
+
 class UpdateDataProcess(QThread):
-    data_changed = Signal(list, float, dict)
+    data_changed = Signal(list, str, str)
 
     def run(self):
-        data_process = []
-        total_memory_usage = 0
-        status_process = {}
         while True:
             sleep(5)
 
-            self.data_changed.emit(data_process, total_memory_usage, status_process)
+            data_process = []
+            total_memory_usage = 0
+            status_process = {}
+            for directory in listdir('/proc'):
+                if not isdir(f'/proc/{directory}') or not format_int(directory):
+                    continue
+
+                proc = Process(directory)
+                data_process.append(proc)
+
+                total_memory_usage += float(proc.memory_usage)
+                if not status_process.get(proc.status):
+                    status_process[proc.status] = 0
+
+                status_process[proc.status] += 1
+
+            percent_memory_usage = format((total_memory_usage * 100) / total_memory_machine(), '.2f')
+
+            text_info_process = f'<b>Total number of processes:</b> {len(data_process)};     '
+            total_status = len(status_process) - 1
+            for index, status in enumerate(status_process):
+                if index == total_status:
+                    text_info_process += f'{status_process.get(status)} are {status}'
+                else:
+                    text_info_process += f'{status_process.get(status)} are {status},   '
+
+            self.data_changed.emit(data_process, percent_memory_usage, text_info_process)
 
 
 class UpdateGeneralInfos(QThread):
--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from PySide6.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QHead
 from PySide6.QtCore import QFile, Slot
 from os import listdir
 from os.path import isdir
-from threading import Thread
 from time import sleep
 
 from models.Process import Process
@@ -18,7 +17,6 @@ class ProcessManager(QMainWindow):
         super().__init__()
 
         self.__data_process = []
-        self.__length_columns = {}
         self.__total_memory_usage = 0
         self.__status_process = {}
         self.__columns_header = [
@@ -39,8 +37,10 @@ class ProcessManager(QMainWindow):
         self.update_data_thread.data_changed.connect(self.update_infos_process)
         self.update_data_thread.start()
 
-        # self.update_thread = Thread(target=self.update_infos_screen)
-        # self.update_thread.daemon = True
+        # atualizar informações dos processos
+        self.update_data_thread = UpdateDataProcess(self)
+        self.update_data_thread.data_changed.connect(self.update_data_process)
+        self.update_data_thread.start()
 
     @staticmethod
     def setup_ui(ui_file_name):
@@ -94,7 +94,6 @@ class ProcessManager(QMainWindow):
         """
         self.__data_process.clear()
         self.__status_process = {}
-        self.__length_columns = {}
         self.__total_memory_usage = 0
         for directory in listdir('/proc'):
             if not isdir(f'/proc/{directory}') or not format_int(directory):
@@ -156,8 +155,6 @@ class ProcessManager(QMainWindow):
         count = 0
         while True:
             sleep(1)
-            self.__label_time.setText(f'<b>Current time:</b> {get_current_time()}')
-
             if count % 5 == 0:
                 count = 0
                 self.load_data_process()
@@ -166,9 +163,26 @@ class ProcessManager(QMainWindow):
 
             count += 1
 
-    @Slot(list, float, dict)
-    def onDataChanged(self, data_process, total_memory_usage, status_process):
-        print('chegou os dados')
+    @Slot(list, str, str)
+    def update_data_process(self, data_process, percent_memory_usage, text_info_process):
+        # while self.__table_list_process.rowCount() > 0:
+        #     self.__table_list_process.removeRow(0)
+        #
+        # for index, proc in enumerate(self.__data_process):
+        #     self.__table_list_process.insertRow(index)
+        #
+        #     self.__table_list_process.setItem(index, 0, QTableWidgetItem(str(proc.pid)))
+        #     self.__table_list_process.setItem(index, 1, QTableWidgetItem(str(proc.user)))
+        #     self.__table_list_process.setItem(index, 2, QTableWidgetItem(str(proc.name)))
+        #     self.__table_list_process.setItem(index, 3, QTableWidgetItem(str(proc.priority)))
+        #     self.__table_list_process.setItem(index, 4, QTableWidgetItem(str(proc.memory_usage)))
+        #     self.__table_list_process.setItem(index, 5, QTableWidgetItem(str(proc.disk_read)))
+        #     self.__table_list_process.setItem(index, 6, QTableWidgetItem(str(proc.disk_write)))
+        #     self.__table_list_process.setItem(index, 7, QTableWidgetItem(str(proc.status)))
+
+        self.__label_memory_usage.setText(f'<b>Total memory used:</b> {percent_memory_usage}%')
+        self.__label_infos_process.setText(text_info_process)
+
         # it = self.tableWidget.item(row, column)
         # if it is None:
         #     it = QtWidgets.QTableWidgetItem()
